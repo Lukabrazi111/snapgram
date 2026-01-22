09d8386cdfb2a25b3761f9061af76d57bd2c8a35
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2026-01-22 23:32:09 +0100

    chore: change base container

--- frontend/src/components/BaseContainer.tsx
+++ frontend/src/components/BaseContainer.tsx
@@ -1,8 +1,6 @@
-export default function BaseContainer({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
+import type { ReactNode } from 'react';
+
+export default function BaseContainer({ children }: { children: ReactNode }) {
     return (
         <div className="w-full max-w-5xl mx-auto mt-14 px-5">{children}</div>
     );
